d17040a6e18a144317920323e85748f52df4af29
jg323 <jgll@aluno.ifnmg.edu.br>
2025-03-09 17:08:26 +0000

    d

--- js/script.js
+++ js/script.js
@@ -32,10 +32,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
     updateCarousel();
     
-    document.getElementById("formContato").addEventListener("submit", function(event) {
-        event.preventDefault();
-        alert("Mensagem enviada com sucesso!");
-        document.getElementById("formContato").reset();
-    });
-</script>
-});
+})
